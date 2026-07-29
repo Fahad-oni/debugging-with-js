75a8cfa7d7c13c2ed8da8520a7e5475e177d2452
Fahad Oni <fahadoni272@gmail.com>
2026-07-29 14:47:22 +0600

    cpmpleted task 14-9a

--- debugging.js
+++ debugging.js
@@ -129,3 +129,12 @@ function countVowels(str) {
 }
 
 console.log(countVowels('aapplee')); // solved
+
+// 14-9A: Temperature Converter
+// Should convert Celsius to Fahrenheit using the formula (C × 9/5) + 32.
+
+function celsiusToFahrenheit(celsius) {
+  return celsius * 9 / 5 + 32; // find the bug
+}
+
+console.log(celsiusToFahrenheit(0)); // Expected: 32
